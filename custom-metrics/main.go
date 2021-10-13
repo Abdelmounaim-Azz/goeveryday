d636bb536040dedc5851a2c8923c2f20b250a645
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-10-13 05:12:09 +0100

    add custom metrics and standard HTTP RED metrics

---
+++ custom-metrics/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// computeSum generates a SHA256 hash of the body
+func computeSum(body []byte) []byte {
+	h := sha256.New()
+	h.Write(body)
+	hashed := hex.EncodeToString(h.Sum(nil))
+	return []byte(hashed)
+}
+func main() {
+	port := 8080
+	readTimeout := time.Millisecond * 500
+	writeTimeout := time.Millisecond * 500
+	mux := http.NewServeMux()
+	hashesInflight := prometheus.NewGauge(prometheus.GaugeOpts{
+		Subsystem: "http",
+		Name:      "hashes_inflight",
+		Help:      "total hashes inflight",
+	})
+	prometheus.MustRegister(hashesInflight)
+	httpMetrics := NewHttpMetrics()
+	customHash := func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if r.Body != nil {
+			defer r.Body.Close()
+			hashesInflight.Inc()
+			defer hashesInflight.Dec()
+			body, _ := ioutil.ReadAll(r.Body)
+			fmt.Fprintf(w, "%s", computeSum(body))
+		}
+	}
+	instrumentedHash := InstrumentHandler(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if r.Body != nil {
+			body, _ := ioutil.ReadAll(r.Body)
+			fmt.Fprintf(w, "%s", computeSum(body))
+		}
+	}, httpMetrics)
+
+	mux.HandleFunc("/hash_red", instrumentedHash)
+	mux.HandleFunc("/hash_custom", customHash)
+	mux.Handle("/metrics", promhttp.Handler())
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: 1 << 20, // Max header of 1MB
+		Handler:        mux,
+	}
+	log.Printf("Listening on: %d", port)
+	s.ListenAndServe()
+}
---
+++ custom-metrics/metrics.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type HttpMetrics struct {
+	RequestsTotal            *prometheus.CounterVec
+	RequestDurationHistogram *prometheus.HistogramVec
+}
+
+func NewHttpMetrics() HttpMetrics {
+	return HttpMetrics{
+		RequestsTotal: promauto.NewCounterVec(prometheus.CounterOpts{
+			Subsystem: "http",
+			Name:      "requests_total",
+			Help:      "total HTTP requests processed",
+		}, []string{"code", "method"}),
+		RequestDurationHistogram: promauto.NewHistogramVec(prometheus.HistogramOpts{Subsystem: "http",
+			Name:    "request_duration_seconds",
+			Help:    "Seconds spent serving HTTP requests.",
+			Buckets: prometheus.DefBuckets,
+		}, []string{"code", "method"}),
+	}
+}
+
+// InstrumentHandler instruments any HTTP handler for the request
+// total and request duration metric
+func InstrumentHandler(next http.HandlerFunc, _http HttpMetrics) http.HandlerFunc {
+	return promhttp.InstrumentHandlerCounter(_http.RequestsTotal,
+		promhttp.InstrumentHandlerDuration(_http.RequestDurationHistogram, next))
+}
